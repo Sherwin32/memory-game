c1f7d480afa94a9ea2cd28a5659074efdf710f1a
Sherwin Shann <sherwin.shann@gmail.com>
2017-09-15 15:35:43 +0800

    created game logic

---
+++ js/main.js
@@ -0,0 +1,8 @@
+//to make sure the js file is connected to index.html
+console.log("Up and running!");
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "queen";
+var cardFour = "queen";
+
+console.log("User flipped " + cardOne);
